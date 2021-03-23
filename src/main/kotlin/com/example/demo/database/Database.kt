51d8a5012005c10f2d110f5b171baec3e50e53ec
Reda <reda.ieslaslagunas@gmail.com>
2021-03-23 22:07:39 +0100

    actualizando bb

--- src/main/kotlin/com/example/demo/database/Database.kt
+++ src/main/kotlin/com/example/demo/database/Database.kt
@@ -5,27 +5,30 @@ import java.sql.DriverManager
 
 /*
 *
-DROP DATABASE IF EXISTS gestacad;
-CREATE DATABASE gestacad;
-DROP USER IF EXISTS administrador;
+
+DROP DATABASE IF EXISTS oficio;
+CREATE DATABASE oficio;
+DROP USER IF EXISTS root;
 CREATE USER root IDENTIFIED BY 'root';
-GRANT ALL PRIVILEGES ON gestacad.* TO root WITH GRANT OPTION;
-USE gestacad;
+GRANT ALL PRIVILEGES ON oficio.* TO root WITH GRANT OPTION;
+USE oficio;
 
-CREATE TABLE person (
-id int primary key auto_increment,
-name VARCHAR(32) not null,
-title VARCHAR(64) NOT NULL,
-date_deleted int unsigned not null default 0)ENGINE=InnoDB;
+CREATE TABLE person
+	(
+    id		INT AUTO_INCREMENT,
+    name VARCHAR(32) not null,
+	title VARCHAR(64) NOT NULL,
+    date_deleted    LONG,
+    PRIMARY KEY(id)
+	)ENGINE=InnoDB
+	;
 
-INSERT INTO person VALUE (0,'Josep Vila','Enginyer informatica');
-INSERT INTO person VALUE (0,'Ninel Mariana','Doctora fisica');
-INSERT INTO person VALUE (0,'Joan marquez','Artista');
+INSERT INTO person VALUE (1,'Axel','Diseñador web', 0);
+INSERT INTO person VALUE (2,'Pere','Programador', 0);
+INSERT INTO person VALUE (3,'Fernando','Director del proyecto', 0);
+
+
 SELECT * FROM person;
-
-ALTER TABLE person CHANGE nom name varchar(32);
-alter table person add column date_delete int unsigned not null default 0 after title;
-alter table person change column  date_deleted date_deleted bigint unsigned default 0;
 *
 * */
 
